7c2c9aba5972448c22b9e7d44b53fa508beca090
Eric Sutherland <ericsutherland@ou.edu>
2021-02-09 16:20:02 -0600

    first commmit

---
+++ WPFClasses/WPFClasses/EntryForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WPFClasses
+{
+    public class EntryForm
+    {
+        public string Name      {get; set;}
+        public string Address   {get; set;}
+        public int ZipCode { get; set; }
+
+        public EntryForm()
+        {
+            Name = string.Empty;
+            Address = string.Empty;
+            ZipCode = 0;
+        }
+
+        public EntryForm(string name, string address, int zipCode)
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
