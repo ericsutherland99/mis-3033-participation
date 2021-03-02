141fa94f4c6008c4e4cd0e7cf93c184fbe34f670
Eric Sutherland <ericsutherland@ou.edu>
2021-03-02 14:27:47 -0600

    finished

--- (P)Json/(P)Json/ClickPokemon.cs
+++ (P)Json/(P)Json/ClickPokemon.cs
@@ -6,14 +6,17 @@ namespace _P_Json
 {
     public class ClickPokemon
     {
-        public int hieght { get; set; }
+        public int height { get; set; }
         public int weight { get; set; }
+        public sprites sprites { get; set; }
+
 
     }
 
     public class sprites
     {
         public string front_default { get; set; }
+        public string back_default { get; set; }
     }
 
 }
--- (P)Json/(P)Json/MainWindow.xaml.cs
+++ (P)Json/(P)Json/MainWindow.xaml.cs
@@ -22,10 +22,10 @@ namespace _P_Json
     /// </summary>
     public partial class MainWindow : Window
     {
+        public bool front = true;
         public MainWindow()
         {
             InitializeComponent();
-
 
             AllPokemonAPI api;
             string url = "https://pokeapi.co/api/v2/pokemon?limit=1200";
@@ -54,10 +54,45 @@ namespace _P_Json
             {
                 string json = client.GetStringAsync(url2).Result;
                 click = JsonConvert.DeserializeObject<ClickPokemon>(json);
+
+                lblheight.Content = click.height;
+                lblweight.Content = click.weight;
+                imgpokemon.Source = new BitmapImage(new Uri(click.sprites.front_default));
             }
 
-            imgpokemon.Source = new BitmapImage(new Uri(sprites.front))
+            
+            
                
         }
+
+        private void btnswitch_Click(object sender, RoutedEventArgs e)
+        {
+            var selecteditem = (resultobject)lstPokemon.SelectedItem;
+            ClickPokemon click;
+            string url2 = selecteditem.url;
+            using (var client = new HttpClient())
+            {
+                string json = client.GetStringAsync(url2).Result;
+                click = JsonConvert.DeserializeObject<ClickPokemon>(json);
+
+                if (click.sprites.back_default is null)
+                {
+                    return;
+                }
+                if (front)
+                {
+                    imgpokemon.Source = new BitmapImage(new Uri(click.sprites.back_default));
+                    front = false;
+                }
+                else
+                {
+                    imgpokemon.Source = new BitmapImage(new Uri(click.sprites.front_default));
+                    front = true;
+                }
+
+            }
+            
+        }
+
     }
 }
