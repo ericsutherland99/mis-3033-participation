71f4bcde9aa1eeb029ea5ead8f0bde3811447875
Eric Sutherland <ericsutherland@ou.edu>
2021-03-12 09:48:40 -0600

    fix error

---
+++ (P) ChuckNorrisJokes/WpfApp1/Chuck Norris.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+   
+
+namespace WpfApp1
+{
+    class Chuck_Norris
+    {
+        public List<Chuck_Norris> categories { get; set; }
+    }
+}
--- (P) ChuckNorrisJokes/WpfApp1/MainWindow.xaml.cs
+++ (P) ChuckNorrisJokes/WpfApp1/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Newtonsoft.Json;
 
 namespace WpfApp1
 {
@@ -23,6 +25,26 @@ namespace WpfApp1
         public MainWindow()
         {
             InitializeComponent();
+
+            populatecategorycombobox();
+        }
+
+        private void populatecategorycombobox()
+        {
+            cmb_category.Items.Add("All");
+            cmb_category.SelectedIndex = 0;
+
+            Chuck_Norris chuck_;
+            string url = "https://api.chucknorris.io/jokes/categories";
+            using (var client = new HttpClient())
+            {
+                string json = client.GetStringAsync(url).Result;
+                chuck_ = JsonConvert.DeserializeObject<Chuck_Norris>(json);
+                foreach (var item in chuck_.categories)
+                {
+                    cmb_category.Items.Add(item);
+                }
+            }
         }
     }
 }
