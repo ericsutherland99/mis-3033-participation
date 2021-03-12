02b5f673fa7545a3d854edbc4ff53f88c942d167
Eric Sutherland <ericsutherland@ou.edu>
2021-03-12 14:42:14 -0600

    list box populated

---
+++ ChuckNorris/ChuckNorris/Chuck.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChuckNorris
+{
+    class Chuck
+    {
+        public string[] categories { get; set; }
+    }
+}
---
+++ ChuckNorris/ChuckNorris/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Newtonsoft.Json;
+
+namespace ChuckNorris
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            populatecategorycombobox();
+        }
+
+        private void populatecategorycombobox()
+        {
+            cmb_category.Items.Add("All");
+            cmb_category.SelectedIndex = 0;
+
+            //Chuck fill;
+            string url = "https://api.chucknorris.io/jokes/categories";
+
+            using (var client = new HttpClient())
+            {
+                string json = client.GetStringAsync(url).Result;
+                string[] categories = JsonConvert.DeserializeObject<string[]>(json);
+                foreach(string category in categories)
+                {
+                    cmb_category.Items.Add(category);
+                }
+            }
+        }
+    }
+}
