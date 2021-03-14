1bdddb42fdd7550bef6757a75a033e70229e0ae7
Eric Sutherland <ericsutherland@ou.edu>
2021-03-14 09:58:44 -0500

    all done!

---
+++ GOTQUOTES/GOTQUOTES/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Newtonsoft.Json;
+
+namespace GOTQUOTES
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_random_Click(object sender, RoutedEventArgs e)
+        {
+            string url = "https://got-quotes.herokuapp.com/quotes";
+            GameOfThronesAPI api;
+
+            using (var client = new HttpClient())
+            {
+                string json = client.GetStringAsync(url).Result;
+                api = JsonConvert.DeserializeObject<GameOfThronesAPI>(json);
+                txtblk_quote.Text = api.quote;
+                lbl_whosaidit.Content = $"-{api.character}";
+            }
+        }
+    }
+}
